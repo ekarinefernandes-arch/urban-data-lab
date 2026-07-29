e4886bc0dd0c9d1348fb867e2acb29ead1d27921
Emmanuely Karine Fernandes da Paz <ekarinefernandes@gmail.com>
2026-07-29 19:49:25 -0300

    Adiciona consulta à API do IBGE e exportação para CSV

--- src/app.py
+++ src/app.py
@@ -1,11 +1,43 @@
-from modules.ibge import consultar_ibge
+import pandas as pd
+
+from modules.ibge import consultar
+
 
 url = (
     "https://servicodados.ibge.gov.br/api/v3/agregados/"
-    "6579/periodos/2022/variaveis/9324?"
-    "localidades=N6[4106902]"
+    "6579/periodos/2021/variaveis/9324?"
+    "localidades=N6[4115200]"
 )
 
-dados = consultar_ibge(url)
+resposta = consultar(url)
+dados = resposta.json()
 
-print(dados)
+variavel = dados[0]["variavel"]
+unidade = dados[0]["unidade"]
+
+serie = dados[0]["resultados"][0]["series"][0]
+
+municipio = serie["localidade"]["nome"]
+ano, valor = next(iter(serie["serie"].items()))
+
+tabela = pd.DataFrame(
+    [
+        {
+            "municipio": municipio,
+            "ano": int(ano),
+            "indicador": variavel,
+            "unidade": unidade,
+            "valor": int(valor),
+        }
+    ]
+)
+
+print(tabela)
+
+tabela.to_csv(
+    "../data/processed/populacao_maringa_2021.csv",
+    index=False,
+    encoding="utf-8-sig"
+)
+
+print("\nArquivo CSV salvo com sucesso!")
--- src/modules/ibge.py
+++ src/modules/ibge.py
@@ -1,14 +1,15 @@
 import requests
 
+BASE_URL = "https://servicodados.ibge.gov.br/api/v3"
 
-def consultar_ibge(url):
+
+def consultar(url):
     """
-    Faz uma consulta à API do IBGE e retorna os dados em formato JSON.
+    Faz uma requisição GET para a API do IBGE.
     """
 
     resposta = requests.get(url)
 
-    if resposta.status_code == 200:
-        return resposta.json()
+    print(f"Status: {resposta.status_code}")
 
-    raise Exception(f"Erro {resposta.status_code} ao consultar a API.")
+    return resposta
